9457b164bacc1e990e0ce86d7ff22c8b6d372b73
Nobuaki Kobayashi <ditch.mouse@gmail.com>
2019-05-18 20:33:31 +0900

    create Asynchronous communication

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,54 @@
+$(function(){
+  function buildHTML(message) {
+    var content = message.content ? `${ message.content }` : "";
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    var html = `<div class="message">
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">
+                      ${message.user_name}
+                    </div>
+                    <div class="upper-message__date">
+                      ${message.date}
+                    </div>
+                  </div>
+                  <div class="lower-message">
+                  <div class="lower-message__content">
+                  ${content}
+                    </div>
+                    ${img}
+                  </div>
+                </div>`
+  return html;
+  }
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('#message_content').val('');
+      $('#message_image').val('');
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+
+    })
+
+    .fail(function(data){
+      alert('エラーが発生したためメッセージは送信できませんでした。');
+    })
+
+    .always(function(data){
+      $('.form__submit').prop('disabled', false);
+    })
+  })
+})
