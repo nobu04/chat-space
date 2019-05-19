5cec609e66afcd49417494e981b8ecea4b18a30f
Nobuaki Kobayashi <ditch.mouse@gmail.com>
2019-05-19 17:33:02 +0900

    Update resorce

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:edit, :update, :create]
+  resources :users, only: [:index, :edit, :update, :create]
   resources :groups, only: [:new, :index, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
